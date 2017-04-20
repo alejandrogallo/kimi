a00313bda208ab7900f88fa1c3c309418a8124ed
Alejandro Gallo <aamsgallo@gmail.com>
2017-04-20 16:30:19 +0200

    Add constants.h

---
+++ src/constants.h
@@ -0,0 +1,10 @@
+#include <string>
+
+namespace  kimi
+{
+
+  static const float bohr_to_angstrom = 0.529177;
+  static const std::string XYZ_INPUT_NAME = "input.xyz";
+  static const std::string CONFIG_FILE_NAME = "config.ini";
+
+} /*  kimi */ 
