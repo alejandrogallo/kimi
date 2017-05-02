912fa27a6906e24857be674833cc04a7ed367b79
Alejandro Gallo <aamsgallo@gmail.com>
2017-05-03 01:03:27 +0200

    Adapt one body integral code from libint example

--- src/utils.cxx
+++ src/utils.cxx
@@ -19,3 +19,65 @@ compute_nuclear_repulsion_energy(std::vector<libint2::Atom>& structure)
   }
   return enuc;
 }
+
+Eigen::MatrixXd
+compute_1body_ints(
+  const libint2::BasisSet shells,
+  libint2::Operator obtype,
+  const std::vector<libint2::Atom>& atoms
+  )
+{
+
+  // Get number of basis set functions
+  int n = shells.nbf();
+  Eigen::MatrixXd result(n,n);
+
+  //// construct the overlap integrals engine
+  libint2::Engine engine(obtype, shells.max_nprim(), shells.max_l(), 0);
+  // nuclear attraction ints engine needs to know where the charges sit ...
+  // the nuclei are charges in this case; in QM/MM there will also be classical charges
+  if (obtype == libint2::Operator::nuclear) {
+    std::vector<std::pair<double,std::array<double,3>>> q;
+    for(const libint2::Atom& atom : atoms) {
+      q.push_back(
+          {static_cast<double>(atom.atomic_number), {{atom.x, atom.y, atom.z}}}
+          );
+    }
+    engine.set_params(q);
+  }
+
+  const std::vector<size_t> shell2bf = shells.shell2bf();
+
+  // buf[0] points to the target shell set after every call  to
+  // engine.compute()
+  const auto& buf = engine.results();
+
+  // loop over unique shell pairs, {s1,s2} such that s1 >= s2
+  // this is due to the permutational symmetry of the real integrals over
+  // Hermitian operators: (1|2) = (2|1)
+  for(auto s1=0; s1!=shells.size(); ++s1) {
+
+    auto bf1 = shell2bf[s1]; // first basis function in this shell
+    auto n1 = shells[s1].size();
+
+    for(auto s2=0; s2<=s1; ++s2) {
+
+      auto bf2 = shell2bf[s2];
+      auto n2 = shells[s2].size();
+
+      // compute shell pair; return is the pointer to the buffer
+      engine.compute(shells[s1], shells[s2]);
+
+      // "map" buffer to a const Eigen Matrix, and copy it to the corresponding
+      // blocks of the result
+      Eigen::Map<const Eigen::MatrixXd> buf_mat(buf[0], n1, n2);
+      result.block(bf1, bf2, n1, n2) = buf_mat;
+      if (s1 != s2) // if s1 >= s2, copy {s1,s2} to the corresponding {s2,s1}
+      //block, note the transpose!
+      result.block(bf2, bf1, n2, n1) = buf_mat.transpose();
+
+    }
+  }
+
+  return result;
+}
--- src/utils.h
+++ src/utils.h
@@ -1,5 +1,13 @@
 #include <vector>
 #include <libint2.hpp>
+#include <Eigen/Dense>
 
 double
 compute_nuclear_repulsion_energy(std::vector<libint2::Atom>& structure);
+
+Eigen::MatrixXd
+compute_1body_ints(
+  const libint2::BasisSet shells,
+  libint2::Operator obtype,
+  const std::vector<libint2::Atom>& atoms
+  );
